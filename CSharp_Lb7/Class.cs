e3b0e110c53922f0f8cec9f6c1c9334bd2ad6e0a
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2023-05-25 11:56:51 +0300

    Rebuild project using .net 7.0

---
+++ CSharp_Lb7/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp_Lb7
+{
+    internal class Track
+    {
+        public string TrackName { get; set; } = string.Empty;
+        public DateTime TrackLength { get; set; } = DateTime.Now;
+    }
+
+    internal class Album
+    {
+        public string AlbumName { get; set; } = string.Empty;
+        public int Year { get; set; } = 0;
+        public int CountTracks { get; set; } = 0;
+        public List<string> Genres { get; set; } = new List<string>();
+        public List<Track> Tracks { get; set; } = new List<Track>();
+    }
+
+    internal class Artist
+    {
+        public string ArtistName { get; set; } = string.Empty;
+        public List<Album> Albums { get; set; } = new List<Album>();
+    }
+}
